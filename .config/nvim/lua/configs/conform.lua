f6f5138a2d53838bc0a398830a253094829d8950
JoaoMuller99 <jp99muller@gmail.com>
2024-12-11 11:41:20 -0300

    chore: conform.lua new config

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -4,12 +4,13 @@ local options = {
     css = { "prettier" },
     html = { "prettier" },
     javascript = { "prettier" },
+    javascriptreact = { "prettier" },
     typescript = { "prettier" },
+    typescriptreact = { "prettier" },
   },
 
   format_on_save = {
     -- These options will be passed to conform.format()
-    timeout_ms = 500,
     lsp_fallback = true,
   },
 }
