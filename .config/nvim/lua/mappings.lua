efc47afeb7e16afb41745fef5e8582c0f226e2c3
JoaoMuller99 <jp99muller@gmail.com>
2024-12-12 11:06:04 -0300

    feat: adicao diagnostic tooltip

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,5 +4,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "gk", ":lua vim.diagnostic.open_float()<CR>", { desc = "Open diagnostic tooltip" })
 
 -- add yours here
