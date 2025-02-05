3e3434920e2e04b71b23527b27d09847e0e71b2e
João Pedro Müller <jp99muller@gmail.com>
2025-02-05 13:17:33 -0300

    chore: remocao organize imports on save

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,10 +1,10 @@
 require "nvchad.options"
 
 vim.api.nvim_set_option("clipboard", "unnamedplus")
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = "*.ts,*.tsx,*.js,*.jsx",
-  command = "OrganizeImports",
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+--   pattern = "*.ts,*.tsx,*.js,*.jsx",
+--   command = "OrganizeImports",
+-- })
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
